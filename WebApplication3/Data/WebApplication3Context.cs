69835a8ef56a66cb138e0c63dc764f3c0833c493
Nguyễn Trung Hiếu <144864144+TrungHieu24@users.noreply.github.com>
2024-01-18 11:03:53 +0700

    add

---
+++ WebApplication3/Data/WebApplication3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication3.Data
+{
+    public class WebApplication3Context : DbContext
+    {
+        public WebApplication3Context (DbContextOptions<WebApplication3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication4.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ WebApplication3/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Phone]
+        public string? Phone { get; set; }
+    }
+}
