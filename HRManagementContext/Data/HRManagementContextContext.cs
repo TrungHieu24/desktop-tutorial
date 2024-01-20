f37b2395fb5949cae4f087c10818ba222d98245a
Nguyễn Trung Hiếu <144864144+TrungHieu24@users.noreply.github.com>
2024-01-20 10:23:03 +0700

    update

---
+++ HRManagementContext/Data/HRManagementContextContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HRManagement.Models;
+
+namespace HRManagementContext.Data
+{
+    public class HRManagementContextContext : DbContext
+    {
+        public HRManagementContextContext (DbContextOptions<HRManagementContextContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HRManagement.Models.Employee> Employee { get; set; } = default!;
+
+        public DbSet<HRManagement.Models.Department> Department { get; set; } = default!;
+    }
+}
